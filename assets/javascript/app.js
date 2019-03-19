b58c74d50080e07f13f3d3cc3d93ffefaa4215d6
Jessica  Gomez <jessicagomez@Jessicas-MacBook-Air.local>
2019-03-18 20:25:16 -0700

    some css

---
+++ assets/javascript/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+//declaring some variables
+ var rightAnswer;
+ var wrongAnswer;
+ var questionsArray;
+
+
+ //score variables
+ var userScoredRight;
+ var userScoredWrong;
+
+
+ //when the user clicks start...
+    //the new layout wll appear
+    //the first question and the possible answers will appear 
+    //the timer will start
+
+//On the first page...
+    //if there is time on the clock and the user clicks on the button with the CORRECT answer it'll show the correct answer page
+    //if the user clicks on the WRONG with time on the clock, then it will take them to the wrong answer page
+    //if the user runs out of time, move on to the next page
+    //this needs to repeat until all the questions have been answered
+
+//On the last page
+    //show the total of correct questions
+    //show the total incorrect questions
+    //shows the number of unasnwered questions
+    //has a button that restarts the game
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
